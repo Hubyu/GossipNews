4a22b8521c270d473507da8fd0292f45e4c10a11
zhanganyong <759659467@qq.com>
2016-07-20 22:00:26 +0800

    热推基本模块搭建。。。

--- app/src/main/java/com/moon/gossipnews/fragment/HotFragment.java
+++ app/src/main/java/com/moon/gossipnews/fragment/HotFragment.java
@@ -1,8 +1,6 @@
 package com.moon.gossipnews.fragment;
 
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,12 +21,17 @@ import com.moon.gossipnews.utils.VolleyUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HotFragment extends Fragment {
     private RecyclerView mRecyclerView;
+    private PtrClassicFrameLayout ptrFl;
     private List<HotBean.DataBean> mDatas = new ArrayList<>();
     private HotAdapter mAdapter;
     public HotFragment() {
@@ -42,6 +45,7 @@ public class HotFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_hot, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.rlv);
+        ptrFl = (PtrClassicFrameLayout) view.findViewById(R.id.ptrFl);
         return view;
     }
 
@@ -51,6 +55,7 @@ public class HotFragment extends Fragment {
         mAdapter = new HotAdapter(getActivity(),mDatas);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        refresh();
         LoaderData();
     }
 
@@ -64,5 +69,30 @@ public class HotFragment extends Fragment {
                 }
             }
         },null,HotBean.class);
+    }
+    private void refresh(){
+        ptrFl.setResistance(1.5f);
+        //是否是下拉刷新
+        ptrFl.setPullToRefresh(false);
+        //头部高度的几倍再去刷新
+        ptrFl.setRatioOfHeaderHeightToRefresh(1.2f);
+        //刷新的时候，是否保持头部
+        ptrFl.setKeepHeaderWhenRefresh(true);
+        //恢复到头部的位置的时长
+        ptrFl.setDurationToClose(200);
+        //头部整体关闭的时常
+        ptrFl.setDurationToCloseHeader(1000);
+        ptrFl.setPtrHandler(new PtrDefaultHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                ptrFl.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.notifyDataSetChanged();
+                        ptrFl.refreshComplete();
+                    }
+                },3000);
+            }
+        });
     }
 }
