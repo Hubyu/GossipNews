b900f2458b4205f0dfb84de842b395d9f7d22b20
zhanganyong <759659467@qq.com>
2016-07-20 20:06:26 +0800

    热推基本模块搭建

---
+++ app/src/main/java/com/moon/gossipnews/contant/UrlStrings.java
@@ -0,0 +1,8 @@
+package com.moon.gossipnews.contant;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class UrlStrings {
+    public static final String URL_HOT = "http://www.jinrongbaguanv.com/jinba/index_articles/1";
+}
